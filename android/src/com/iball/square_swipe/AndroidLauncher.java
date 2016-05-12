3317b06e4bffb65b3d8bb107ef7dd94c0943c4aa
SamONelson <S.OliverNelson@gmail.com>
2016-05-12 14:28:36 -0400

    Advertisement init()
    haven't implemented the load video as it needs a listener and stuff. I
    can't figure out how to call the showFullScreenAd() in the GameManager
    class, as we want to call the one respective to whether its Desktop or
    Android.

--- android/src/com/iball/square_swipe/AndroidLauncher.java
+++ android/src/com/iball/square_swipe/AndroidLauncher.java
@@ -2,22 +2,39 @@ package com.iball.square_swipe;
 
 import android.os.Bundle;
 
+import com.apptracker.android.track.AppTracker;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.iball.square_swipe.Main;
 import com.iball.square_swipe.services.LeadBolt;
 
 public class AndroidLauncher extends AndroidApplication implements LeadBolt {
+
+	// Leadbolt SDK configurations
+	private static final String APP_API_KEY 		    = "is2byYEVjbXiFjVjaYIt6sM4aEIqMWZ3"; // change this to your App specific API KEY
+	private static final String LOCATION_CODE		    = "inapp";
+	private static final String LOCATION_CODE_VIDEO 	= "video";
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Main(), config);
+
+		if(savedInstanceState == null) {
+			// Initialize LeadBolt SDK with your api key
+			AppTracker.startSession(getApplicationContext(),APP_API_KEY,AppTracker.ENABLE_AUTO_CACHE);
+		}
+		// cache LeadBolt Ad without showing it
+		AppTracker.loadModuleToCache(getApplicationContext(),LOCATION_CODE);
+
 	}
 
 	@Override
 	public void showFullscreenAd() {
-		
+		if(AppTracker.isAdReady(LOCATION_CODE)&&AppTracker.isInternetAvailable(this)) {
+			AppTracker.loadModule(getApplicationContext(),LOCATION_CODE);
+		}
 	}
 
 	@Override
